1c14cbcb4905fe554535314fc2b531f341f98977
zulfianprasetyo <you@example.com>
2023-05-20 21:53:13 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import (
     Flask, 
     request, 
@@ -13,12 +16,26 @@ import requests
 from datetime import datetime
 from bson import ObjectId
 
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+
+
 app = Flask(__name__)
 
-cxn_str = 'mongodb+srv://zulfian506150700:zulfian@cluster0.ksi9p44.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient(cxn_str)
+# cxn_str = 'mongodb+srv://zulfian506150700:zulfian@cluster0.ksi9p44.mongodb.net/?retryWrites=true&w=majority'
+# client = MongoClient(cxn_str)
 
-db = client.dbsparta_plus_week2
+# db = client.dbsparta_plus_week2
 
 @app.route('/')
 def main():
@@ -147,7 +164,7 @@ def delete_ex():
 
    
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=3000, debug=True)
 
 
 
